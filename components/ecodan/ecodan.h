314a3331d12437ee4a10bf20338886a539dd7b9c
Jesse van Leth <jessevanleth@outlook.com>
2024-05-11 12:34:02 +0200

    Update ecodan.h

--- components/ecodan/ecodan.h
+++ components/ecodan/ecodan.h
@@ -20,7 +20,7 @@ namespace ecodan
 
     class EcodanHeatpump : public PollingComponent, public esphome::api::CustomAPIDevice {
     public:        
-        EcodanHeatpump() : PollingComponent(15000) {}
+        EcodanHeatpump() : PollingComponent(30000) {}
         void set_rx(int rx);
         void set_tx(int tx);
         void setup() override;
